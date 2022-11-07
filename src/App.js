bca153b804c36f121703758037de8efd80ec6e0b
Your Name <you@example.com>
2022-11-07 23:59:21 +0600

    file setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn w-64 rounded-full">Button</button>
+     
     </div>
   );
 }
