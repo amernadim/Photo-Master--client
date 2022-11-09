c15bd4aba894470ee290fcce2e657bddc21ec996
Your Name <you@example.com>
2022-11-09 11:39:44 +0600

    added conditional review fild

--- src/component/Pages/Services/ServiceDetails.jsx
+++ src/component/Pages/Services/ServiceDetails.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Context/AuthProvider";
 
 const ServiceDetails = () => {
+  const {user} = useContext(AuthContext);
   const serviceInfo = useLoaderData();
-  // console.log(serviceInfo);
+ 
   const { _id, service, price, rating, image, photoshoot, description } =
-    serviceInfo;
+  serviceInfo;
+  
+  // console.log(_id,user?.email,user?.displayName,user?.photoURL);
+
+  const handleReview = event => {
+    event.preventDefault()
+    const form = event.target;
+    const review = form.review.value;
+
+    const reviewInfo = {
+      serviceId : _id,
+      serviceName : service,
+      reviewerName : user.displayName,
+      reviewerEmail : user.email,
+      reviewerPhoto : user?.photoURL,
+      reviewText : review,
+    }
+
+    console.log(reviewInfo);
+
+
+  }
+
+
 
   return (
     <>
@@ -23,7 +48,13 @@ const ServiceDetails = () => {
       </div>
     </section>
 
-    <section className="review-section my-5">
+    <hr className="border-2 bg-black"/>
+    <div className="my-5">
+      <h2 className="text-2xl font-bold text-center text-orange-500">Review</h2>
+    </div>
+
+    <section className="review-section grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-11/12 mx-auto my-10 ">
+
       
     <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 bg-white dark:text-gray-100">
 	<div className="flex justify-between p-4">
@@ -47,8 +78,79 @@ const ServiceDetails = () => {
 		<p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
 		<p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
 	</div>
-</div>
+    </div>
+
+    <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 bg-white dark:text-gray-100">
+	<div className="flex justify-between p-4">
+		<div className="flex space-x-4">
+			<div>
+				<img src="https://source.unsplash.com/100x100/?portrait" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+			</div>
+			<div>
+				<h4 className="font-bold">Leroy Jenkins</h4>
+				<span className="text-xs dark:text-gray-400">2 days ago</span>
+			</div>
+		</div>    
+		<div className="flex items-center space-x-2 dark:text-yellow-500">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current">
+				<path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+			</svg>
+			<span className="text-xl font-bold">4.5</span>
+		</div>
+	</div>
+	<div className="p-4 space-y-2 text-sm dark:text-gray-400">
+		<p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
+		<p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
+	</div>
+    </div>
+
+    <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 bg-white dark:text-gray-100">
+	<div className="flex justify-between p-4">
+		<div className="flex space-x-4">
+			<div>
+				<img src="https://source.unsplash.com/100x100/?portrait" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+			</div>
+			<div>
+				<h4 className="font-bold">Leroy Jenkins</h4>
+				<span className="text-xs dark:text-gray-400">2 days ago</span>
+			</div>
+		</div>
+		<div className="flex items-center space-x-2 dark:text-yellow-500">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current">
+				<path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+			</svg>
+			<span className="text-xl font-bold">4.5</span>
+		</div>
+	</div>
+	<div className="p-4 space-y-2 text-sm dark:text-gray-400">
+		<p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
+		<p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
+	</div>
+    </div>
+
     </section>
+
+    <section className="user-review-fild my-4">
+    
+    { 
+    user?.uid ?
+
+       <div className="max-w-screen-xl gap-8 px-8 py-16 mx-auto border-2 border-black rounded-lg md:px-12 lg:px-10 xl:px-6 w-10/12 md:w-6/12">	
+       <h2 className="text-4xl font-bold leading-tight lg:text-3xl">Let's send your opinion!</h2> 
+       <form onSubmit={handleReview} className="space-y-6 mt-3">
+      <div>
+       <textarea type="text" name="review" rows="3" className="w-full p-3 rounded dark:bg-gray-800"></textarea>
+     </div>
+     <button type="submit" className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-orange-400 text-white">Send Review</button>
+      </form>
+     </div>
+     : 
+     <h2 className="text-xl font-bold text-center text-orange-500">Please signin to add a review</h2>
+
+    }   
+    </section>
+
+    
     </>
   );
 };
